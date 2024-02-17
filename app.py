0fb8fd99126cd4bb0b2d5b34a93110b9fff63f53
Jordan Manus <94319835+jordan-manus@users.noreply.github.com>
2024-02-17 13:33:46 -0500

    added skeleton endpoints.

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello, World!"
+
+@app.route('/success/<name>')
+def success(name):
+    return 'welcome %s' %name
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    # call database to verify if user is present
+
+    #if not present in DB, redirect to register account
+
+    #else return valid token and redirect to homepage
+
+
+
+# allows the user to register a brand new account
+@app.route('/register', methods=['POST'])
+def register():
+    pass
+
+
+# allows the user to create/edit their initial questionnaire 
+@app.route('/questionnaire', methods=['POST', 'PUT'])
+def questionnaire():
+    pass
+
+
+# allows the user to create and retrieve items from DB
+@app.route('/items', methods=['POST', 'GET'])
+def items():
+    pass
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
